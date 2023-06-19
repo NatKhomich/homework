bd24608df6f2203705ebd02965dbb6fbe4d062c1
NatKhomich <yalapulya94@mail.ru>
2023-06-19 20:53:42 +0600

    homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,12 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return state
+            if (action.payload === 'up') {
+               return [...state].sort((a,b) => a.name.localeCompare(b.name))
+        } else if (action.payload === 'down') {
+             return  [...state].sort((a,b) => b.name.localeCompare(a.name))
+            }
+           else return state
         }
         case 'check': {
             return state.filter(el => el.age >= action.payload) // need to fix
