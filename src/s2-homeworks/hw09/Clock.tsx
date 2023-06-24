d271db72cccc1f76b2577596586c3fce5df691b0
NatKhomich <yalapulya94@mail.ru>
2023-06-24 12:58:52 +0600

    homework 9 style SuperButton

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -12,7 +12,8 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-        setTimerId(+setInterval(() => setDate(new Date()), 1000))
+        const id = +setInterval(() => setDate(new Date()), 1000)
+        setTimerId(id)
     }
 
     const stop = () => {
